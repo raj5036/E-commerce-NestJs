fa02ce66244063641252c5b8666d467729c65371
Raj Karmakar <97rajmath@gmail.com>
2024-06-22 14:51:10 +0530

    Created basic logic for registering new user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { AuthDTO } from './dto';
+import * as argon from 'argon2';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class AuthService {
+	constructor(private prisma: PrismaService) {}
 	async login(authDTO: AuthDTO) {
 		console.log(authDTO);
 		return 'login';
 	}	
 
 	async register(authDTO: AuthDTO) {
-		console.log(authDTO);
-		return 'register1';
+		try {
+			const hashedPassword = await argon.hash(authDTO.password);
+			const newUser = await this.prisma.user.create({
+				data: {
+					email: authDTO.email,
+					password: hashedPassword,
+					firstname: authDTO.firstname,
+					lastname: authDTO.lastname,
+				}, 
+				select: {
+					id: true,
+					email: true,
+				}
+			})
+			return newUser;
+		} catch (error) {
+			if (error instanceof PrismaClientKnownRequestError) {
+				if (error.code === 'P2002') {
+					throw new ForbiddenException('Credentials taken');
+				} else if (error.code === 'P2003') {
+					throw new ForbiddenException('Credentials taken');
+				}
+			}
+
+			throw error
+		}
 	}
 }
