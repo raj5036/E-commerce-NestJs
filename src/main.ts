096fc5f996b56447fe33bd30354a155296dcb1ae
Raj Karmakar <97rajmath@gmail.com>
2024-06-25 21:37:20 +0530

    Created SSL Keys and integrated it with the App

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as fs from 'fs';
+import * as path from 'path';
+// import * as https from 'https';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync(path.resolve('src/ssl/server.key')),
+    cert: fs.readFileSync(path.resolve('src/ssl/server.cert')),
+  };
+
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions
+  });
+
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true
   }))
   await app.listen(3000);
+
+  console.log(`Application is running on: ${process.env.HOST}:${process.env.PORT}`);
 }
 bootstrap();
